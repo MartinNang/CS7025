c1eaa3319c3445ebea58fc0406421604d877a444
MartinNang <53534948+MartinNang@users.noreply.github.com>
2024-09-26 12:52:12 +0100

    Create loops_and_conditions.js

---
+++ loops_and_conditions.js
@@ -0,0 +1,70 @@
+let isPetAwake = true;
+let henry = {
+    petName : "Henry",
+    furColors : ["grey", "white"]
+}
+
+function feedPet(pet) {
+    console.log(`${pet.petName} has been fed.`);
+}
+
+function walkPet(pet) {
+    console.log(`${pet.petName} has been walked.`);
+}
+
+function testConditions(pet, time) {
+    console.log("time", time);
+    if (time >= 9 && time < 14) {
+        feedPet(pet);
+    }
+    else if (time >= 14 && time < 20) {
+        walkPet(pet);
+    }
+    else {
+        console.log(pet.petName + " is asleep.");
+    }
+}
+
+function testForLoop(pet) {
+    for (let i = 0; i < pet.furColors.length; i++) {
+        let furColor = pet.furColors[i];
+        console.log(furColor);
+    }
+}
+
+function testForInLoop(pet) {
+    for (furColor in pet.furColors) {
+        console.log(pet.furColors[furColor]);
+    }
+}
+
+function testWhileLoop(pet) {
+    let i = 0;
+    while (i < pet.furColors.length) {
+        let furColor = pet.furColors[i];
+        console.log(furColor);
+        i++;
+    }
+}
+
+function testDoWhileLoop(pet) {
+    let i = 0;
+    do {
+        let furColor = pet.furColors[i];
+        console.log(furColor);
+        i++;
+    }
+    while (i < pet.furColors.length);
+}
+
+testConditions(henry, 10);
+testConditions(henry, 15);
+testConditions(henry, 20);
+
+testForLoop(henry);
+testForInLoop(henry);
+testDoWhileLoop(henry);
+
+
+
+
